def797a25e38a2aa2826085297a1f5df33fd2f68
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-08-26 06:30:22 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3
-
-# simple Flask web application that displays 'Hello HBNB!' on the root route.
+#!/usr/bin/python3
+""" Flask web application """
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
